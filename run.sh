a16521fc90a9fe57e0d68b9b39426e9397e919f5
Aman Agarwal <amanag.11@gmail.com>
2021-05-21 18:44:12 +0530

    Add halt condition

--- run.sh
+++ run.sh
@@ -2,7 +2,10 @@
 sudo yum update
 
 # inference
-rm latest_numerai_tournament_data.csv.xz
 wget 'https://numerai-public-datasets.s3-us-west-2.amazonaws.com/latest_numerai_tournament_data.csv.xz'
 /opt/python3/bin/python3 predict.py
 /opt/python3/bin/python3 send_email.py
+rm latest_numerai_tournament_data.csv.xz
+
+# shuts down instance after making predictions
+sudo halt
